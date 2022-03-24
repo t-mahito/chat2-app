584a9a49d61d939bf7417cc74a93d46a74f1a3f3
t-mahito <qqna42p9@gmail.com>
2022-03-24 12:42:31 +0900

    空のメッセージが送信できないバリデーションの設定

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Chat2App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
-    config.time_zone = "Tokyo"
+    config.time_zone = 'Tokyo'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
